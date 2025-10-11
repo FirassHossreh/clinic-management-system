35718d705fc2a14d4aa6b2d39368b9cc06612cb2
FirassHossreh <firass.hus@gmail.com>
2025-10-11 23:03:46 +0300

    remove connection with google

--- backend/app.js
+++ backend/app.js
@@ -1,63 +1,32 @@
-const dotenv = require("dotenv");
-dotenv.config();
 
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dataBaseConnection = require("./configs/database-connection");
 const authRoute = require("./routes/authRoute");
-const googleAuthRoute = require("./routes/googleAuthRoute");
-const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const passport = require("passport"); // Bu satır eksikti!
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL'iniz
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 if (process.env.ENVIRONMENT === "development") {
   app.use(morgan("dev"));
 }
-app.use(cookieParser());
-app.use(
-  session({ secret: "keyboard cat", resave: false, saveUninitialized: true })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+
+
 
 dataBaseConnection();
 
 app.use(`/api/${process.env.API_VERSION || "v1"}/`, authRoute);
-app.use(`/api/${process.env.API_VERSION || "v1"}/auth`, googleAuthRoute);
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const dataBaseConnection = require("./configs/database-connection");
-const doctor = require("./models/doctor");
-const authRoute = require("./routes/authRoute");
-dotenv.config();
 
-app.use(express.json());
-
-if (process.env.ENVIRONMENT === "development") {
-  app.use(morgan("dev"));
-}
-
-dataBaseConnection();
-app.get("/", async (req, res) => {
-  const newDoctor = new doctor({
-    firstName: "Dr. John",
-    lastName: "Doe",
-    email: "john.doe@example.com",
-    password: "password123",
-  });
-  await newDoctor.save();
-  res.send("backend is running...");
-});
-app.use(`/api/${process.env.API_VERSION || "v1"}/`, authRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
